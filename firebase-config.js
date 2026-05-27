a8180b971dfa3401760dc85764aef770b87544f6
Johnny <johnny@iMac-de-amorim.local>
2026-05-27 09:39:18 -0300

    Adiciona configuracao Firebase

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCEB_7hQT0zzHtgve-KLPqpZMtImErVDyU", 
+  authDomain: "amr-software.firebaseapp.com",
+  projectId: "amr-software",
+  storageBucket: "amr-software.firebasestorage.app",
+  messagingSenderId: "125374662743",
+  appId: "1:125374662743:web:77b4cf6139c07747f97b12"
+};
